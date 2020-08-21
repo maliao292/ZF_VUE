61c03816669997f33cf941cccd4eb899a553ed03
maliao292 <maming292@163.com>
2020-08-21 17:41:18 +0800

    xiabna

---
+++ 模拟vue响应式原理/myVue.js
@@ -0,0 +1,30 @@
+class Vue {
+    constructor(options) {
+        //1. 通过属性保存选项数据
+        this.$options = options || {}
+        this.$data = options.data || {}
+        this.$el = typeof options.el === 'string' ? document.querySelector('el') : options.el
+        //2. 把data中的成员转换成 getter和setter 注入到vue实例
+
+        //3. 调用observer对象，监听数据变化
+        //4. 调用compiler对象，解析指令和差值变化
+        this._proxyData(this.$data)
+    }
+    _proxyData(data) {
+        // 遍历data中的属性
+        Object.keys(data).forEach(key => {
+            Object.defineProperty(this, key, {
+                enumerable: true,
+                configurable: true,
+                get() {
+                    return data[key]
+                },
+                set(newValue) {
+                    if (newValue = data[key]) return
+                    data[key] = newValue
+                }
+            })
+        });
+        // 把data 是属性注入到vue 实例
+    }
+}
